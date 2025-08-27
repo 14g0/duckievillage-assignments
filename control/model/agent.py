b94079afb55c507124eeb49da786e27cec7ad4a3
14g0 <iagocesarts@gmail.com>
2025-08-27 18:31:49 -0300

    tarefa pela metade

--- control/model/agent.py
+++ control/model/agent.py
@@ -1,8 +1,8 @@
 # MAC0318 Intro to Robotics
 # Please fill-in the fields below with your info
 #
-# Name:
-# NUSP:
+# Name: Iago Cesar Tavares de Souza
+# NUSP: 17466770
 #
 # ---
 #
@@ -52,8 +52,8 @@ class Agent:
         self.baseline = 0.102 # [m]
 
         # Motor constants
-        self.motor_gain = 0.01 # K_m -- you should modify this value
-        self.motor_trim = 0.0  # K_t -- you should modify this value
+        self.motor_gain = 0.053  # K_m -- you should modify this value
+        self.motor_trim = -0.01   # K_t -- you should modify this value
 
         key_handler = key.KeyStateHandler()
         environment.unwrapped.window.push_handlers(key_handler)
